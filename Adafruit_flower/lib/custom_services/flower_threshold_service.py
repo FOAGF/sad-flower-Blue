cabf527c73ff899c150d25311f4c0d341ca369d6
Ben Thorpe <b.thorpe@uqconnect.edu.au>
2023-05-19 12:26:38 +0000

    Add custom service for flower thresholds
    Custom ble service which allows for the reading and writing of the
    flowers threshold values for determining what state the flower should
    be in.

---
+++ Adafruit_flower/lib/custom_services/flower_threshold_service.py
@@ -0,0 +1,51 @@
+"""
+`custom_service.flower_threshold_service`
+================================================================================
+
+BLE control of CSSE4011 flower threshold values, these includes CO2 and VTOC
+min/max values, how many air quality levels there are, and how the levels are
+calculated.
+
+* Author(s): Ben Thorpe
+"""
+
+from adafruit_ble.characteristics import Characteristic
+from adafruit_ble.characteristics.int import Uint8Characteristic, Uint16Characteristic
+from adafruit_ble.services import Service
+from adafruit_ble.uuid import VendorUUID
+
+
+class FlowerThresholdService(Service):
+    """Control threshold levels of flower, min CO2, max CO2 etc"""
+
+    uuid = VendorUUID("C55E4011-C55E-4011-0001-C55E40110000")
+
+    minCO2 = Uint16Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0001-C55E40110001"),
+        properties=(Characteristic.READ | Characteristic.WRITE),
+        initial_value=400,
+    )
+
+    maxCO2 = Uint16Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0001-C55E40110002"),
+        properties=(Characteristic.READ | Characteristic.WRITE),
+        initial_value=2000,
+    )
+
+    minVTOC = Uint16Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0001-C55E40110003"),
+        properties=(Characteristic.READ | Characteristic.WRITE),
+        initial_value=0,
+    )
+
+    maxVTOC = Uint16Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0001-C55E40110004"),
+        properties=(Characteristic.READ | Characteristic.WRITE),
+        initial_value=200,
+    )
+
+    mode = Uint8Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0001-C55E40110005"),
+        properties=(Characteristic.READ | Characteristic.WRITE),
+        initial_value=0,
+    )
