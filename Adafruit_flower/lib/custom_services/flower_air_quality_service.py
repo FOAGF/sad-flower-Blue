5eed2659fb4af5cd5f5fd62537cf38cb05917e76
Ben Thorpe <b.thorpe@uqconnect.edu.au>
2023-05-20 10:06:58 +0000

    Add flower air quality service

---
+++ Adafruit_flower/lib/custom_services/flower_air_quality_service.py
@@ -0,0 +1,41 @@
+"""
+`custom_service.flower_air_quality_service`
+================================================================================
+
+BLE service for reading current air quality metrics of the flower,
+can also be used to set the level of the flower.
+
+* Author(s): Ben Thorpe
+"""
+
+from adafruit_ble.characteristics import Characteristic
+from adafruit_ble.characteristics.int import Uint8Characteristic, Uint16Characteristic
+from adafruit_ble.services import Service
+from adafruit_ble.uuid import VendorUUID
+
+
+class FlowerAirQualityService(Service):
+    """Read current air quality metrics of the flower,
+    can also be used to set the flower level directly"""
+
+    uuid = VendorUUID("C55E4011-C55E-4011-0002-C55E40110000")
+
+    co2ppm = Uint16Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0002-C55E40110001"),
+        properties=(Characteristic.READ),
+        initial_value=400,
+    )
+
+    vtocppb = Uint16Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0002-C55E40110002"),
+        properties=(Characteristic.READ),
+        initial_value=0,
+    )
+
+    current_level = Uint16Characteristic(
+        uuid=VendorUUID("C55E4011-C55E-4011-0002-C55E40110003"),
+        properties=(Characteristic.READ | Characteristic.WRITE),
+        initial_value=0,
+    )
+
+
